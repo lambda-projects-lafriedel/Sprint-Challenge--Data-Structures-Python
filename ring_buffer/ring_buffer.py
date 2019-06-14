74ab5c5e7dc27fddf01ddd80768893cc9328e175
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-14 11:18:51 -0400

    Refactor code and pass tests for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,21 +5,21 @@ class RingBuffer:
         self.storage = [None]*capacity
 
     def append(self, item):
-        # adds elements to the buffer
-        # if current == len(self.storage) - 1, reset current to 0
-        if current == capacity - 1:
-            current = 0
-        # append item to storage at current, increase current by 1
-        self.storage.insert(current, item)
-        current += 1
+        # if current (index) == capacity, reset current to 0
+        if self.current == self.capacity:
+            self.current = 0
+        # remove item at current, append item to storage at current
+        self.storage.pop(self.current)
+        self.storage.insert(self.current, item)
+        # increase current by 1
+        self.current += 1
 
 
     def get(self):
-        # returns all elements in a list in their given order
-        # do not return any None values, even if they are present
         elements = list()
         # for each element in list, if not None, append to new list
         for item in self.storage:
+            # do not return any None values, even if they are present
             if item is None:
                 continue
             elements.append(item)
